c7eced73d0b7a50c284d69a65c60b18f5e0ec2dd
Miroslaw01 <70401026+Miroslaw01@users.noreply.github.com>
2022-09-26 13:46:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-zdljlw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-zdljlw)
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+@NgModule({
+  imports:      [ 
+    BrowserModule,
+  ],
+
+  declarations: [ 
+    AppComponent,
+    HeaderComponent,
+    RecipeComponent,
+    ShoppingListComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+  ],
+
+  bootstrap:    [ 
+    AppComponent, 
+  ]
+})
+export class AppModule { }
