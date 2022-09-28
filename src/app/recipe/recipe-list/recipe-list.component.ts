6064bc71a9e4daf4842e7a3d331909df4a43bb44
Miroslaw01 <70401026+Miroslaw01@users.noreply.github.com>
2022-09-28 14:09:14 +0200

    6 files modified, 2 added

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 import { Recipe } from '../recipe.model';
 
@@ -12,21 +13,29 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
     new Recipe(
-    'mhmh',
-    'mhmh',
-    'https://i.kym-cdn.com/entries/icons/original/000/022/134/elmo.jpg'
-  ),
-  new Recipe(
-    'mhmh2',
-    'mhmh2',
-    'https://c.tenor.com/3z4G6A1_vpwAAAAM/monty-python-black-knight.gif'
-  ),
-  new Recipe(
-    'mhmh3',
-    'mhmh3',
-    'https://static.giga.de/wp-content/uploads/2018/01/y-tho-meme.jpg'
-  ) 
-];
+      'mhmh1',
+      'mhmh1',
+      'https://i.kym-cdn.com/entries/icons/original/000/022/134/elmo.jpg',
+      [
+        new Ingredient('Memes', 3),
+        new Ingredient('MEMEE', 5)
+      ]
+    ),
+    new Recipe(
+      'mhmh2',
+      'mhmh2',
+      'https://c.tenor.com/3z4G6A1_vpwAAAAM/monty-python-black-knight.gif',
+      [
+        new Ingredient('Test', 2)
+      ]
+    ),
+    new Recipe(
+      'mhmh3',
+      'mhmh3',
+      'https://static.giga.de/wp-content/uploads/2018/01/y-tho-meme.jpg',
+      []
+    ),
+  ];
 
   selectedRecipe: Recipe;
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [];
   constructor() { }
 
   ngOnInit() {
