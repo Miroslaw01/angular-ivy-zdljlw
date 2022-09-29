c10988862cda1f7cefbed68fcc712947790b6ae2
Miroslaw01 <70401026+Miroslaw01@users.noreply.github.com>
2022-09-29 13:20:21 +0200

    2 files modified

--- src/app/dropdown.directive.ts
+++ src/app/dropdown.directive.ts
@@ -1,6 +1,7 @@
 import { Directive, HostBinding, HostListener } from '@angular/core';
 @Directive({
   selector: '[appDropdown]',
+  exportAs: 'appDropdown',
 })
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
