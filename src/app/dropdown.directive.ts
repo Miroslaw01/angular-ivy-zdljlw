ecd5a1e7fb5b994a097e9eed12801dce0d443291
Miroslaw01 <70401026+Miroslaw01@users.noreply.github.com>
2022-09-29 08:48:04 +0200

    3 files modified, 1 added

---
+++ src/app/dropdown.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, HostBinding, HostListener } from "@angular/core";
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  isOpen = false;
+@HostBinding
+
+
+  @HostListener('click') toggle(){
+    this.isOpen = !this.isOpen;
+  }
+}
